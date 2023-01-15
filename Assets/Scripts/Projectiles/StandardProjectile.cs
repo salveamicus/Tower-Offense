b4ffdde520a15a94ff644fa69a410894f8e3bd3f
Archaversine <adam.brohl.w@gmail.com>
2023-01-15 11:59:43 -0600

    Replaced LineRenderer with SpriteRenderer

--- Assets/Scripts/Projectiles/StandardProjectile.cs
+++ Assets/Scripts/Projectiles/StandardProjectile.cs
@@ -4,35 +4,27 @@ using UnityEngine;
 
 public class StandardProjectile : MonoBehaviour
 {
-    // Temporary, will later be replaced with some kind of arrow sprite
-    public LineRenderer lineRenderer;
-    public Vector3 velocity = Vector2.zero;
-
-    public int sides = 5;
-    public float radius = 0.1f;
+    public Vector3 velocity = Vector3.zero;
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += velocity;
-
-        DrawPolygon();
-    }
-
-    // Code from: https://www.youtube.com/watch?v=cRulHmoiabA
-    // Draws a polygon with the line renderer
-    void DrawPolygon()
-    {
-        const float TAU = 2 * Mathf.PI;
-
-        lineRenderer.positionCount = sides;
-
-        for (int i = 0; i < sides; ++i)
+        if (velocity != Vector3.zero)
         {
-            float rad = TAU * ((float) i / sides);
-            lineRenderer.SetPosition(i, radius * new Vector3(Mathf.Cos(rad), Mathf.Sin(rad), 0) + transform.position);
-        }
+            transform.position += velocity;
 
-        lineRenderer.loop = true;
+            // Gets the angle to rotate the sprite by by using trig ( tan angle = y/x ).
+            // Offset by 90 degrees because the sprite is facing up in the png
+            // Atan2: Finds the angle of a vector whose angle = y / x
+            // Rad2Deg: Equal to 180 degrees / pi Radians (Converts radians to degrees)
+            float degrees = Mathf.Atan2(velocity.y, velocity.x) * Mathf.Rad2Deg - 90;
+
+            // I have no idea wehat eulerAngles means, but if you set its value to x, it will
+            // set the rotation to x degrees.
+            // Vector3.forward: Shorthand for writing new Vector3(0, 0, 1) -- This is done because
+            //  The sprite is being rotated about the z axis (imagine z axis as depth of the screen)
+            //  so Vector3.forward * degrees is the same as: new Vector3(0, 0, degrees)
+            transform.eulerAngles = Vector3.forward * degrees;
+        }
     }
 }
