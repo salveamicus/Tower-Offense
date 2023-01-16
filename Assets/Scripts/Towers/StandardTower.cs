edad1adb9b331b26c095e7249fa3d1b1ca9e0f2e
Archaversine <adam.brohl.w@gmail.com>
2023-01-16 11:48:32 -0600

    Added Standard Tower

---
+++ Assets/Scripts/Towers/StandardTower.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardTower : MonoBehaviour
+{
+    public StandardProjectile Projectile;
+    public float ProjectileSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot(Vector3 direction)
+    {
+        StandardProjectile p = Instantiate(Projectile, transform.position, transform.rotation);
+        p.velocity = direction.normalized * ProjectileSpeed;
+    }
+}
