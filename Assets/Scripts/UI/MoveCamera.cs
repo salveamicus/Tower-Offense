61179bcaa4a6dd7f490f26f3e12ca5cf813c6573
jwosborne-knox <jwosborne@knox.edu>
2023-01-16 22:04:46 +0000

    improved camera pan

--- Assets/Scripts/UI/MoveCamera.cs
+++ Assets/Scripts/UI/MoveCamera.cs
@@ -6,17 +6,27 @@ using UnityEngine.UIElements;
 public class MoveCamera : MonoBehaviour
 {
     public float speed;
-    private void Start()
-    {
-        Debug.Log("Screen Height : " + Screen.height);
-        Debug.Log("Screen Width : " + Screen.width);
-        Debug.Log("Mouse Position : (" + Input.mousePosition.x + ", " + Input.mousePosition.y + ")");
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.mousePosition.x >= Screen.width || Input.GetKey("right"))
+        if ((Input.mousePosition.y >= Screen.height && Input.mousePosition.x >= Screen.width) || (Input.GetKey("right") && Input.GetKey("up")))
+        {
+            transform.position += speed * (Vector3.right + Vector3.up);
+        }
+        else if ((Input.mousePosition.y >= Screen.height && Input.mousePosition.x <= 0 )|| (Input.GetKey("left") && Input.GetKey("up")))
+        {
+            transform.position += speed * (Vector3.left + Vector3.up);
+        }
+        else if ((Input.mousePosition.y <= 0 && Input.mousePosition.x >= Screen.width) || (Input.GetKey("right") && Input.GetKey("down")))
+        {
+            transform.position += speed * (Vector3.right + Vector3.down);
+        }
+        else if ((Input.mousePosition.y <= 0 && Input.mousePosition.x <= 0) || ((Input.GetKey("left") && Input.GetKey("down") )))
+        {
+            transform.position += speed * (Vector3.left + Vector3.down);
+        }
+        else if (Input.mousePosition.x >= Screen.width || Input.GetKey("right"))
         {
             transform.position += speed * Vector3.right;
         } 
