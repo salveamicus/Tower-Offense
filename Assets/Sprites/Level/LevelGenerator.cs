4d5ce6f5006a6d662d37c827ac5903bffdc8aa75
Archaversine <adam.brohl.w@gmail.com>
2023-01-23 18:48:07 -0600

    Created LevelGenerator Object

---
+++ Assets/Sprites/Level/LevelGenerator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GrandTower grandTower;
+    public StandardTower standardTower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
