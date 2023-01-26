f38744f021bac7c25b3fa84a125aab279f6ad643
ewbrees <81719511+ewbrees@users.noreply.github.com>
2023-01-26 13:19:23 -0600

    Added tutorial and some bug fixes

--- Assets/Scripts/Scenes/MainMenu.cs
+++ Assets/Scripts/Scenes/MainMenu.cs
@@ -12,6 +12,6 @@ public class MainMenu : MonoBehaviour
 
     public void TutorialGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Tutorial");
     }
 }
