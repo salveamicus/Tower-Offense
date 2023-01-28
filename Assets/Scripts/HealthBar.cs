a1a2a25cb03118b1d7f6f4cf7f0f80a74a9a9a25
Archaversine <adam.brohl.w@gmail.com>
2023-01-28 16:10:46 -0600

    Health bar fix

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,6 +8,7 @@ public class HealthBar : MonoBehaviour
     public float barWidth = 0.6f;
     public float height = 1f;
     public void ChangeHealth(float ratio) {
+        ratio = (ratio < 0) ? 0 : ratio; // To prevent negative health issues ~ Adam
         transform.position = new Vector3((ratio-1)/2*barWidth, transform.position.y, 0);
         transform.localScale = new Vector3(ratio*barWidth, transform.localScale.y, transform.localScale.z);
     }
