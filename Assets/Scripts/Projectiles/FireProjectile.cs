a2421e2289753cec896cd89d280b32ccd2b8a4de
Archaversine <adam.brohl.w@gmail.com>
2023-02-03 19:15:10 -0600

    Fire Tower & Projectile

---
+++ Assets/Scripts/Projectiles/FireProjectile.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireProjectile : Projectile
+{
+    public float LifetimeSeconds = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();       
+        body.isKinematic = true; // Disables velocity and stuff
+
+        Invoke("Die", LifetimeSeconds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Velocity != Vector3.zero)       
+        {
+            transform.position += Velocity * Time.deltaTime;
+            float degrees = Mathf.Atan2(Velocity.y, Velocity.x) * Mathf.Rad2Deg - 90;
+
+            transform.eulerAngles = Vector3.forward * degrees;
+        }
+    }
+}
