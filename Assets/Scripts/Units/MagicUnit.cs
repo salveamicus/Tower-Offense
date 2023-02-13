d7e13a52fdcfcabd106c8981e500aa3b038b0d84
Archaversine <adam.brohl.w@gmail.com>
2023-02-13 13:02:38 -0600

    Magic Unit Prefab

---
+++ Assets/Scripts/Units/MagicUnit.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicUnit : Unit
+{
+    public Projectile projectile;
+
+    public Vector3 moveGoal;
+    private Vector3 zAdjustedGoal;
+
+    public float projectileSpeed = 3f;
+
+    public float maxHealth = 60f;
+    public float health = 60f;
+
+    public float shootRadius = 5f;
+    public float shootCooldownSeconds = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        selectionCircle.SetActive(isSelected);
+
+        // For movement
+        movement(moveGoal);
+
+        UpdateFireTime();
+        UpdatePoisonTime();
+
+        if (health <= 0) Destroy(gameObject);
+
+        UpdateDecceleratorCount();
+        UpdateRangeRadius(shootRadius);
+        ShowRangeIfMouseHover();
+        ShootIfPossible(shootRadius, shootCooldownSeconds);
+
+        // Turn towards closest tower
+        Tuple<float, Vector3> target = GetClosestTarget();
+        Vector3 directionVector = target.Item2 - transform.position;
+
+        float degrees = Mathf.Atan2(directionVector.y, directionVector.x) * Mathf.Rad2Deg + 180;
+        transform.eulerAngles = Vector3.forward * degrees;
+
+        healthBar.transform.position = transform.position + new Vector3((health/maxHealth-1)/2*0.6f, 0.4f, 0);
+        healthBar.transform.rotation = Quaternion.identity;
+    }
+
+    public override void Shoot(Vector3 direction)
+    {
+        Projectile p = Instantiate(projectile, transform.position + Vector3.back, transform.rotation);
+        p.Velocity = direction.normalized * projectileSpeed * SpeedMultiplier;
+        p.OwnerTag = tag;
+    }
+
+    public override void Damage(float amount)
+    {
+        health -= amount;
+        transform.GetChild(1).GetComponent<HealthBar>().ChangeHealth(health/maxHealth);
+    }
+
+    public override void Heal(float amount)
+    {
+        health = MathF.Min(health + amount, maxHealth);
+        transform.GetChild(1).GetComponent<HealthBar>().ChangeHealth(health/maxHealth);
+    }
+
+}
