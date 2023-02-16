a64c2680f8f81a39122c72cd8cc1cfbc1680ecdd
Archaversine <adam.brohl.w@gmail.com>
2023-02-16 13:16:43 -0600

    Added Explosive Projectile (without explosion)

---
+++ Assets/Scripts/Projectiles/ExplosiveProjectile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveProjectile : Projectile
+{
+    public float LifetimeSeconds = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+        body.isKinematic = true;
+
+        Invoke("Die", LifetimeSeconds);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Velocity != Vector3.zero)
+        {
+            transform.position += Velocity * Time.deltaTime;
+            float degrees = Mathf.Atan2(Velocity.y, Velocity.x) * Mathf.Rad2Deg - 90;
+
+            transform.eulerAngles = Vector3.forward * degrees;
+        }
+    }
+
+    protected override void OnHitUnit(Unit unit)
+    {
+        // TODO: Add explosion effect here
+    }
+}
