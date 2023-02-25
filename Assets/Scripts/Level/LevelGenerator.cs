e07214181290f582fee8a66ee039ec7cb8424a90
Archaversine <adam.brohl.w@gmail.com>
2023-02-25 14:33:26 -0600

    Multiple towers generating per level

--- Assets/Scripts/Level/LevelGenerator.cs
+++ Assets/Scripts/Level/LevelGenerator.cs
@@ -107,25 +107,28 @@ public class LevelGenerator : MonoBehaviour
     {
         ++currentLevel;
 
-        int currentRing = dna.Length - dna.Replace("/", "").Length;
-
-        try
+        for (int i = 0; i < Random.Range(2, 4); ++i)
         {
-            string outerRing = dna.Substring(0, dna.Length - 1);
-            if (outerRing.LastIndexOf('/') != -1) outerRing = outerRing.Substring(outerRing.LastIndexOf('/'));
+            int currentRing = dna.Length - dna.Replace("/", "").Length;
 
-            if (outerRing.Length < innerRingCapacity * currentRing * ringCapacityMultiplier)
+            try
             {
-                dna = dna.Substring(0, dna.Length - 1) + GetRandomTowerSymbol() + "/";
+                string outerRing = dna.Substring(0, dna.Length - 1);
+                if (outerRing.LastIndexOf('/') != -1) outerRing = outerRing.Substring(outerRing.LastIndexOf('/'));
+
+                if (outerRing.Length < innerRingCapacity * currentRing * ringCapacityMultiplier)
+                {
+                    dna = dna.Substring(0, dna.Length - 1) + GetRandomTowerSymbol() + "/";
+                }
+                else
+                {
+                    dna += GetRandomTowerSymbol() + "/";
+                }
             }
-            else
+            catch
             {
-                dna += GetRandomTowerSymbol() + "/";
+                dna = dna.Substring(0, Mathf.Max(0, dna.Length - 1)) + GetRandomTowerSymbol() + "/";
             }
-        }
-        catch
-        {
-            dna = dna.Substring(0, Mathf.Max(0, dna.Length - 1)) + GetRandomTowerSymbol() + "/";
         }
     }
 
