9ce21dc76a1ecdf7d868045b13910e369b76e635
ewbrees <81719511+ewbrees@users.noreply.github.com>
2023-02-27 15:29:00 -0600

    small balance touches

--- Assets/Scripts/gameStatistics.cs
+++ Assets/Scripts/gameStatistics.cs
@@ -16,7 +16,7 @@ public class gameStatistics : MonoBehaviour
     public static bool regeneratingLevel = false;
 
     // units
-    public static int[] unitCosts = {75, 100, 200, 250, 300, 350, 600}; // knight, sniper, support, mage, demo, cultist, heavy in order
+    public static int[] unitCosts = {75, 100, 200, 300, 275, 350, 600}; // knight, sniper, support, mage, demo, cultist, heavy in order
 
     public static float fireDamage = 10f;
     public static float poisonDamage = 15f;
