3a36f615efbdf43400dc57e5a6c0629a45754686
ewbrees <81719511+ewbrees@users.noreply.github.com>
2023-03-03 10:23:53 -0600

    touch ups and menu updates

---
+++ Assets/Scripts/Scenes/ReturnToMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    public void Return_To_Menu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
